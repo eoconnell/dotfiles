6f5f729e5e5e1b6aa107d60c30db8b2866356d30
Evan O'Connell <oconn.e@gmail.com>
2024-02-26 18:51:08 -0800

    wezterm theme

---
+++ wezterm/.config/wezterm/theme.lua
@@ -0,0 +1,7 @@
+local module = {}
+
+function module.decorate(config)
+  config.color_scheme = 'Fog Dark'
+end
+
+return module
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
+local theme = require 'theme'
 
 local config = wezterm.config_builder()
+
+theme.decorate(config)
 
 config.hide_tab_bar_if_only_one_tab = true
 
